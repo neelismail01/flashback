28a2bbf91dd5b28f8c6498d98013837516fc27a3
Neel Ismail <neelismail01@gmail.com>
2021-01-21 17:12:17 -0500

    Getting images from server displaying on client

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const knex = require('knex')({
@@ -15,6 +16,8 @@ const knex = require('knex')({
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/uploads', express.static(`${__dirname}/public`));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -44,35 +47,26 @@ app.post('/post', (req, res) => {
         .catch(err => console.log(err));
 
         console.log("File successfully uploaded");
-        return res.status(200).send(req.file);
+        return res.status(200).send("File successfully uploaded");
     })
 })
 
 app.get('/feed/:id', (req, res) => {
-
     knex('posts').where({
         'user_id': parseInt(req.params.id.substring(3))
     })
     .select('img_path')
     .then(paths => {
-        return res.json({numPosts: paths.length});
+        for (let i = 0; i < paths.length; ++i) {
+            if (paths[i].img_path === null) {
+                paths.splice(i, 1);
+            }
+        }
+        console.log(paths);
+        return res.json(paths);
     })
     .catch(err => {
-        console.log("err");
-    });
-})
-
-app.get('/feed/:id/:postNum', (req, res) => {
-
-    knex('posts').where({
-        'user_id': parseInt(req.params.id.substring(3)),
-    })
-    .select('img_path')
-    .then(paths => {
-        return res.json({numPosts: paths.length});
-    })
-    .catch(err => {
-        console.log("err");
+        console.log(err);
     });
 })
 
--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Post from '../Post/Post';
 import Search from '../Search/Search';
@@ -6,23 +6,16 @@ import Feed from '../Feed/Feed';
 import './Home.css';
 
 const Home = (props) => {
-    const handleFeed = (event) => {
-        axios.get(`http://localhost:5000/feed/id=${props.userId}`)
-        .then(res => {
-            for (let i = 0; i < res.data.numPosts; ++i) {
-                
-            }
-        }).catch(err => {
-            console.log(err);
-        });
-    }
+    const [imgLinks, setImgLinks] = useState();
+    const [feedCards, setFeedCards] = useState();
+
+    
 
     return (
         <div className="home">
             <Search />
             <div className="welcome-feed">
-                <Feed onClick={handleFeed} />
-                <input type="button" value="Get Feed" onClick={handleFeed} />
+                <Feed />
             </div>
             <Post />
         </div>
