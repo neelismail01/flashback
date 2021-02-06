108ac5f88b7866c84db488ab2f95a284b063352d
Neel Ismail <neelismail01@gmail.com>
2021-02-06 17:16:26 -0500

    Search queries now being sent to backend

--- front-end/src/components/Navigation/Navigation.js
+++ front-end/src/components/Navigation/Navigation.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Modal from '../Modal/Modal';
+import axios from 'axios';
 import './Navigation.css';
-import './search-icon.jpeg';
 
 const Navigation = (props) => {
     const [showModal, setShowModal] = useState(false);
     const [query, setQuery] = useState('');
+    const [error, setError] = useState(null);
 
     const openModal = () => {
         setShowModal(true);
@@ -16,7 +17,22 @@ const Navigation = (props) => {
     }
 
     const handleChange = event => {
-        setQuery(event.target.value);
+        if (query.length + event.target.value.length <= 50) {
+            setQuery(event.target.value);
+        } else {
+            setError('Please limit your search to 50 characters');
+        }
+    }
+
+    const handleSearch = event => {
+        if (event.key === 'Enter') {
+            console.log(query);
+            axios.get(`http://localhost:5000/search/${props.userId}?search=${query}`, { params: {query: query} })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(err => console.log(err))
+        }
     }
 
     return (
@@ -28,6 +44,7 @@ const Navigation = (props) => {
                         className="search-bar"
                         placeholder="Search"
                         onChange={handleChange}
+                        onKeyPress={handleSearch}
                     />
                     <input
                         type="button"
